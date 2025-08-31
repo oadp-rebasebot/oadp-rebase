9a13eea94ef3c938a55db9d47fff63369227a6bd
Michal Pryc <mpryc@redhat.com>
2025-08-31 21:15:28 +0200

    Go replace for kopia oadp-1.5 branch
    Signed-off-by: Michal Pryc <mpryc@redhat.com>

---
+++ rebasebot-hook-scripts/go-replace_kopia_oadp-1.5.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -euo pipefail
+
+DOWNSTREAM_BRANCH="oadp-1.5"
+DOWNSTREAM_MODULE="github.com/migtools/kopia"
+UPSTREAM_MODULE="github.com/kopia/kopia"
+
+GO_MOD_FILE="go.mod"
+REPLACE_LINE="replace $UPSTREAM_MODULE => $DOWNSTREAM_MODULE $DOWNSTREAM_BRANCH"
+
+# Replace existing line or append if not present
+if grep -q "^replace $UPSTREAM_MODULE" "$GO_MOD_FILE"; then
+    sed -i "s|^replace $UPSTREAM_MODULE.*|$REPLACE_LINE|" "$GO_MOD_FILE"
+else
+    echo "$REPLACE_LINE" >> "$GO_MOD_FILE"
+fi
+
