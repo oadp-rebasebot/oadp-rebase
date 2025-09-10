dfc02f1ef155dc0c517958418d743471e47f24fc
Michal Pryc <mpryc@redhat.com>
2025-09-10 13:21:12 +0200

    Add velero replace for the kubevirt velero plugin
    Signed-off-by: Michal Pryc <mpryc@redhat.com>

--- rebase-configs/migtools_kubevirt_velero_plugin_main.env.sh
+++ rebase-configs/migtools_kubevirt_velero_plugin_main.env.sh
@@ -1,3 +1,9 @@
 SOURCE_UPSTREAM_REPO="https://github.com/kubevirt/kubevirt-velero-plugin:main"
 DESTINATION_DOWNSTREAM_REPO="migtools/kubevirt-velero-plugin:main"
 REBASE_REPO="oadp-rebasebot/kubevirt-velero-plugin:rebase-bot-main"
+
+HOOK_SCRIPTS_LOCATION="git:https://github.com/oadp-rebasebot/oadp-rebase/oadp-dev:rebasebot-hook-scripts"
+HOOK_SCRIPTS="--post-rebase-hook \
+  ${HOOK_SCRIPTS_LOCATION}/go-replace_velero_oadp-dev.sh \
+  ${HOOK_SCRIPTS_LOCATION}/go-mod-tidy-and-commit.sh \
+  "
