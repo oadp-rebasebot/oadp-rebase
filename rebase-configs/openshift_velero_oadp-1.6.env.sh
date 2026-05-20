33a3c51e221990fd873c612704483711e7c0e798
Joseph <jvaikath@redhat.com>
2026-05-20 11:25:20 -0400

    Update velero upstream tag to v1.18.1 for oadp-1.6
    Signed-off-by: Joseph <jvaikath@redhat.com>

--- rebase-configs/openshift_velero_oadp-1.6.env.sh
+++ rebase-configs/openshift_velero_oadp-1.6.env.sh
@@ -1,9 +1,9 @@
 # ===============================================================
 # Velero rebase configuration for OADP 1.6
-# Uses upstream Velero v1.18.1-rc.2 tag
+# Uses upstream Velero v1.18.1 tag
 # ===============================================================
 
-UPSTREAM_VELERO_BRANCH="v1.18.1-rc.2"
+UPSTREAM_VELERO_BRANCH="v1.18.1"
 DESTINATION_DOWNSTREAM_VELERO_BRANCH="oadp-1.6"
 
 SOURCE_UPSTREAM_REPO="https://github.com/velero-io/velero:$UPSTREAM_VELERO_BRANCH"
